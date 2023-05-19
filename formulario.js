fb6f5e9d9e95315f7774dd5b50177369dd9bbde5
BrianFDrakmo <113211108+BrianFDrakmo@users.noreply.github.com>
2023-05-19 01:47:09 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -9,6 +9,13 @@
             e.preventDefault();
         }
     }
+    
+     const validarApellido = function(e){
+        if(formulario.apellido.value == 0){
+            alert("Complete el campo apellido."),
+            e.preventDefault();
+        }
+    }
 
     const validarRadio = function(e){
         if(formulario.sexo[0].checked == true || formulario.sexo[1].checked == true){
@@ -27,6 +34,7 @@
     
     const validar = function(e){
         validarNombre(e);
+        validarApellido(e);
         validarRadio(e);
         validarCalidad(e);
     }
