33ea5e325033bdfcb2da414eb7eddcb9be5c4fc9
Zoe Tavella <133027332+ZoeTav@users.noreply.github.com>
2023-05-25 17:03:45 -0300

    Update apiinstagram.js

--- apiinstagram.js
+++ apiinstagram.js
@@ -5,7 +5,7 @@ const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 
 const token = 'IGQVJVNjFXazNFOUdTY1loclVTLXJPUjFkX0NRYzZAKTW5pdWpVeXhVeEF4RVg0dkZAyU2tob2RLd2lpV09uYzdob2tldWo5U0lTMEs1ZA2lHZAzR0VzZA1QWdwUWY1ZAzd3UWc0Y016c1FrWGNxd2dGVWxtOAZDZD';
-const url= `https://graph.instagram.com/v11.0/17841449614731486/media?fields=business_discovery.username({ceramic.art_proyecto}){media_url,caption,permalink&limit=80}&access_token=${token}`;
+const url= `https://graph.instagram.com/{17841449614731486}?fields=thumbnail_url,media_url,caption,permalink&limit=80&access_token=${token}`;
 
 
 fetch(url)
